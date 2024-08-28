b1e1340031b88ded88c9ee8910a5a5acd9353d67
Radin6 <30755101+Radin6@users.noreply.github.com>
2024-08-28 10:14:58 +0200

    todas las rutas movidas a /controllers

---
+++ backend/src/controllers/chat.controllers.js
@@ -0,0 +1,5 @@
+import { ChatModel } from "../models/chat.model.js";
+
+export const getChatById = async (req,res) => {}
+
+export const createChat = async (req,res) => {}
--- backend/src/routes/products.router.js
+++ backend/src/routes/products.router.js
@@ -1,114 +1,21 @@
 import { Router } from "express";
-import { ProductModel } from "../models/product.model.js";
+import { getAllProducts, getProductById, createProduct, updateProductById, deleteProductById } from "../controllers/product.controllers.js";
 
 const app = Router();
 
-//  Traer todos los productos.
-app.get('/', async (req, res) => {
+// GET all products ✅
+app.get('/', getAllProducts);
 
+// GET all products by id 🚧
+app.get('/:id', getProductById);
 
-    try {
-        const { limit = 10, page = 1, query = '' } = req.query
-        let filter = {};
+// POST create product ✅
+app.post('/', createProduct);
 
-        if (query) {
-            filter = {
-                $or: [
-                    { title: { $regex: query, $options: 'i' } },
-                    { category: { $regex: query, $options: 'i' } }
-                ]
-            };
-        }
+// PUT update product by id 🚧
+app.put('/:id', updateProductById);
 
-
-        const options = {
-            limit: Number(limit),
-            page: Number(page)
-        }
-
-        const result = await ProductModel.paginate(filter, options);
-        const linkPage = (page) => {
-            return `/api/products/?limit=${limit}&page=${page}&query=${query}`;
-        }
-        res.status(200).json({
-            status: 'success',
-            payload: result.docs,
-            totalPages: result.totalPages,
-            prevPage: result.prevPage,
-            nextPage: result.nextPage,
-            page: result.page,
-            hasPrevPage: result.hasPrevPage,
-            hasNextPage: result.hasNextPage,
-            prevLink: result.hasPrevPage ? linkPage(result.prevPage) : null,
-            nextLink: result.hasNextPage ? linkPage(result.nextPage) : null
-        })
-
-
-    } catch (error) {
-        res.json({
-            status: "Falló.", error: error
-        })
-    }
-});
-
-//  Traer un producto por id.
-app.get('/:id', async (req, res) => {
-    try {
-        res.json({
-            status: "Exitoso."
-        })
-    } catch (error) {
-        res.json({
-            status: "Falló."
-        })
-    }
-});
-
-// Crear un producto.
-app.post('/', async (req, res) => {
-
-    try {
-        const { title, price, description, productImage, category } = req.body;
-        if (!title || !description || !productImage || !price || !category) {
-            return res.status(400).json({
-                status: "error", mensaje: "Debe ingresar todos los campos."
-            })
-        }
-        const product = await ProductModel.create({ title, price, description, productImage, category })
-        res.status(201).json({
-            status: "Exitoso.", producto: product
-        });
-    } catch (error) {
-        res.status(500).json({
-            status: "Falló.", error: error
-        })
-    }
-});
-
-//  Modificar un producto.
-app.put('/', async (req, res) => {
-    try {
-        res.json({
-            status: "Exitoso."
-        })
-    } catch (error) {
-        res.json({
-            status: "Falló."
-        })
-    }
-});
-
-// Eliminar un producto.
-app.delete('/', async (req, res) => {
-    try {
-        res.json({
-            status: "Exitoso."
-        })
-    } catch (error) {
-        res.json({
-            status: "Falló."
-        })
-    }
-});
+// DELETE product by id 🚧
+app.delete('/:id', deleteProductById);
 
 export default app;
