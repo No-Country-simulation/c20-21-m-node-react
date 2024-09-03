4e2845432efc9e2e23ca7ca0a7cd6347b4f2dacc
LizTH09 <lsat-1699@hotmail.com>
2024-09-03 14:44:18 -0500

    Guardando las imagenes en servicio externo: cloudinary

--- backend/src/controllers/product.controllers.js
+++ backend/src/controllers/product.controllers.js
@@ -1,34 +1,32 @@
 import { ProductModel } from "../models/product.model.js";
-import { processImagesToBase64 } from "../utils/imageProcessor.js";
-
+import { uploadImages } from "../utils/cloudinary.util.js";
+import fs from "fs-extra";
 
 export const getAllProducts = async (req, res) => {
-
   try {
-    const { limit = 10, page = 1, query = '' } = req.query
+    const { limit = 10, page = 1, query = "" } = req.query;
     let filter = {};
 
     if (query) {
       filter = {
         $or: [
-          { title: { $regex: query, $options: 'i' } },
-          { category: { $regex: query, $options: 'i' } }
-        ]
+          { title: { $regex: query, $options: "i" } },
+          { category: { $regex: query, $options: "i" } },
+        ],
       };
     }
 
-
     const options = {
       limit: Number(limit),
-      page: Number(page)
-    }
+      page: Number(page),
+    };
 
     const result = await ProductModel.paginate(filter, options);
     const linkPage = (page) => {
       return `/api/products/?limit=${limit}&page=${page}&query=${query}`;
-    }
+    };
     res.status(200).json({
-      status: 'success',
+      status: "success",
       payload: result.docs,
       totalPages: result.totalPages,
       prevPage: result.prevPage,
@@ -37,104 +35,145 @@ export const getAllProducts = async (req, res) => {
       hasPrevPage: result.hasPrevPage,
       hasNextPage: result.hasNextPage,
       prevLink: result.hasPrevPage ? linkPage(result.prevPage) : null,
-      nextLink: result.hasNextPage ? linkPage(result.nextPage) : null
-    })
-
-
+      nextLink: result.hasNextPage ? linkPage(result.nextPage) : null,
+    });
   } catch (error) {
     res.json({
-      status: "error", message: "Error al intentar obtener todos los productos" , error: error
-    })
+      status: "error",
+      message: "Error al intentar obtener todos los productos",
+      error: error,
+    });
   }
-}
+};
 
 export const getProductById = async (req, res) => {
   try {
     const { productId } = req.params;
     const product = await ProductModel.findOne({
-      _id: productId
-    })
+      _id: productId,
+    });
 
     if (!product) {
       return res.status(404).json({
-        status: "error", message: "No se encontró el producto", error: error
-      })
+        status: "error",
+        message: "No se encontró el producto",
+        error: error,
+      });
     }
 
-    res.status(200).json(product)
-
+    res.status(200).json(product);
   } catch (error) {
     res.status(400).json({
-      status: "error", message: "Error al buscar el producto", error: error
-    })
+      status: "error",
+      message: "Error al buscar el producto",
+      error: error,
+    });
   }
-}
+};
 
-export const createProduct = async (req, res) => {
-
+export const createProduct = async (req, res, next) => {
   try {
     const { title, price, description, category } = req.body;
-    const productImage = await req.files.map(file => file.buffer)
-    const processedImages = await processImagesToBase64(productImage)
-    if (!title || !description || !productImage || !price || !category) {
+    if (!req.files || !req.files.productImage) {
       return res.status(400).json({
-        status: "error", message: "Debe ingresar todos los campos."
-      })
+        status: "error",
+        message: "No se ha recibido una imagen para subir.",
+      });
     }
-    const product = await ProductModel.create({ title, price, description, productImage:processedImages, category })
+    const images = Array.isArray(req.files.productImage)
+      ? req.files.productImage
+      : [req.files.productImage];
+    const uploadedImages = await Promise.all(
+      images.map(async (file) => {
+        const result = await uploadImages(file.tempFilePath);
+        await fs.unlink(file.tempFilePath);
+        return result;
+      })
+    );
+    req.body.productImage = uploadedImages.map((image) => ({
+      public_id: image.public_id,
+      secure_url: image.secure_url,
+    }));
+
+    if (!title || !description || !uploadedImages || !price || !category) {
+      return res.status(400).json({
+        status: "error",
+        message: "Debe ingresar todos los campos.",
+      });
+    }
+    const product = await ProductModel.create({
+      title,
+      price,
+      description,
+      productImage: req.body.productImage,
+      category,
+    });
     res.status(201).json({
-      status: "success", message: "El producto ha sido creado", data: product
+      status: "success",
+      message: "El producto ha sido creado",
+      data: product,
     });
   } catch (error) {
-    res.status(500).json({
-      status: "error", message: "Error al intentar crear el producto",error: error
-    })
+    next(error);
   }
-}
+};
 
 export const updateProductById = async (req, res) => {
   try {
     const { productId } = req.params;
     const updates = req.body;
-    console.log(productId, updates)
-    const updatedProduct = await ProductModel.findOneAndUpdate({ _id: productId }, updates, { new: true })
+    console.log(productId, updates);
+    const updatedProduct = await ProductModel.findOneAndUpdate(
+      { _id: productId },
+      updates,
+      { new: true }
+    );
 
     if (!updatedProduct) {
       return res.status(404).json({
-        status: "error", message: "No se encontró el producto"
-      })
+        status: "error",
+        message: "No se encontró el producto",
+      });
     }
 
-    res.status(200).json(
-      { status: "success", message: "El producto ha sido actualizado", data: updatedProduct }
-    )
-
+    res.status(200).json({
+      status: "success",
+      message: "El producto ha sido actualizado",
+      data: updatedProduct,
+    });
   } catch (error) {
     res.status(400).json({
-      status: "error", message: "Le búsqueda es incorrecta", error: error
-    })
+      status: "error",
+      message: "Le búsqueda es incorrecta",
+      error: error,
+    });
   }
-}
-
+};
 
 export const deleteProductById = async (req, res) => {
   try {
     const { productId } = req.params;
-    const deletedProduct = await ProductModel.findOneAndDelete({ _id: productId })
+    const deletedProduct = await ProductModel.findOneAndDelete({
+      _id: productId,
+    });
 
     if (!deletedProduct) {
       return res.json({
-        status: "error", message: "No se encontró el producto"
-      })
+        status: "error",
+        message: "No se encontró el producto",
+      });
     }
 
-    return res.status(200).json(
-      { status: "success", message: "El producto ha sido borrado", data: deletedProduct }
-    )
-
+    return res.status(200).json({
+      status: "success",
+      message: "El producto ha sido borrado",
+      data: deletedProduct,
+    });
   } catch (error) {
     res.json({
-      status: "error", message: "Error al intentar borrar el producto", error: error
-    })
+      status: "error",
+      message: "Error al intentar borrar el producto",
+      error: error,
+    });
   }
-}
+};
