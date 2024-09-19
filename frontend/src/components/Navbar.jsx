105981a5e32e71501f1d9192d05ef534da84578f
LizTH09 <lsat-1699@hotmail.com>
2024-09-19 02:32:36 -0500

    Agregando Mis Publicaciones al navbar, en icono de perfil

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,54 +1,71 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Badge, MenuItem, Menu, Box, InputBase, CssBaseline, Button } from '@mui/material';
-import { createTheme, ThemeProvider, styled, alpha } from '@mui/material/styles';
-import MailIcon from '@mui/icons-material/Mail';
-import NotificationsIcon from '@mui/icons-material/Notifications';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import AccountCircle from '@mui/icons-material/AccountCircle';
-import MenuIcon from '@mui/icons-material/Menu';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import SearchIcon from '@mui/icons-material/Search';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import { useNavigate } from 'react-router-dom';
-import { CartContext } from '../context/CartContext';
+import React, { useState, useEffect, useContext } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Badge,
+  MenuItem,
+  Menu,
+  Box,
+  InputBase,
+  CssBaseline,
+  Button,
+} from "@mui/material";
+import {
+  createTheme,
+  ThemeProvider,
+  styled,
+  alpha,
+} from "@mui/material/styles";
+import MailIcon from "@mui/icons-material/Mail";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import MenuIcon from "@mui/icons-material/Menu";
+import MoreIcon from "@mui/icons-material/MoreVert";
+import SearchIcon from "@mui/icons-material/Search";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import { useNavigate } from "react-router-dom";
+import { CartContext } from "../context/CartContext";
 
-const Search = styled('div')(({ theme }) => ({
-  position: 'relative',
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
   borderRadius: theme.shape.borderRadius,
   backgroundColor: alpha(theme.palette.common.white, 0.15),
-  '&:hover': {
+  "&:hover": {
     backgroundColor: alpha(theme.palette.common.white, 0.25),
   },
   marginRight: theme.spacing(2),
   marginLeft: 0,
-  width: '100%',
-  [theme.breakpoints.up('sm')]: {
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
     marginLeft: theme.spacing(3),
-    width: 'auto',
+    width: "auto",
   },
 }));
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
+const SearchIconWrapper = styled("div")(({ theme }) => ({
   padding: theme.spacing(0, 2),
-  height: '100%',
-  position: 'absolute',
-  pointerEvents: 'none',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-  color: 'inherit',
-  '& .MuiInputBase-input': {
+  color: "inherit",
+  "& .MuiInputBase-input": {
     padding: theme.spacing(1, 1, 1, 0),
     // paddingLeft: `calc(1em + ${theme.spacing(4)})`,
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-      width: '20ch',
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
     },
   },
 }));
@@ -56,31 +73,34 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
 const NavBar = ({ onSearch }) => {
   const [anchorEl, setAnchorEl] = useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
   const [darkMode, setDarkMode] = useState(false);
-  const { cartItems } = useContext(CartContext); 
+  const { cartItems } = useContext(CartContext);
   const navigate = useNavigate();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isGuest, setIsGuest] = useState(false);
-  const totalItemsInCart = cartItems.reduce((acc, item) => acc + item.quantity, 0);
+  const totalItemsInCart = cartItems.reduce(
+    (acc, item) => acc + item.quantity,
+    0
+  );
   useEffect(() => {
     const token = localStorage.getItem("token");
-    const guest = localStorage.getItem("guest"); 
+    const guest = localStorage.getItem("guest");
 
     if (token) {
       setIsAuthenticated(true);
-      setIsGuest(false); 
+      setIsGuest(false);
     } else if (guest) {
-      setIsGuest(true); 
+      setIsGuest(true);
     }
   }, []);
 
   useEffect(() => {
-    const savedTheme = localStorage.getItem('theme');
-      if (savedTheme) {
-        setDarkMode(savedTheme === 'dark');
-      }
-    }, []);
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      setDarkMode(savedTheme === "dark");
+    }
+  }, []);
 
   const isMenuOpen = Boolean(anchorEl);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
@@ -107,64 +127,69 @@ const NavBar = ({ onSearch }) => {
   };
 
   const handleSearchSubmit = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       onSearch(searchText);
     }
   };
 
   const theme = createTheme({
     palette: {
-      mode: darkMode ? 'dark' : 'light',
+      mode: darkMode ? "dark" : "light",
     },
   });
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
-    localStorage.setItem('theme', darkMode ? 'light' : 'dark');
+    localStorage.setItem("theme", darkMode ? "light" : "dark");
+  };
+
+  const handleMyPosts = () => {
+    navigate("/post");
   };
 
   const handleLogout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("guest");
-    navigate('/');
+    navigate("/");
   };
 
-  const menuId = 'primary-search-account-menu';
+  const menuId = "primary-search-account-menu";
   const renderMenu = (
     <Menu
       anchorEl={anchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={menuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMenuOpen}
       onClose={handleMenuClose}
     >
       <MenuItem onClick={handleMenuClose}>Perfil</MenuItem>
       <MenuItem onClick={handleMenuClose}>Mi cuenta</MenuItem>
+      <MenuItem onClick={handleMyPosts}>Mis Publicaciones</MenuItem>
       <MenuItem onClick={handleLogout}>Cerrar Sesión</MenuItem>
     </Menu>
   );
 
-  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const mobileMenuId = "primary-search-account-menu-mobile";
   const renderMobileMenu = (
     <Menu
       anchorEl={mobileMoreAnchorEl}
       anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       id={mobileMenuId}
       keepMounted
       transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
+        vertical: "top",
+        horizontal: "right",
       }}
       open={isMobileMenuOpen}
       onClose={handleMobileMenuClose}
@@ -194,7 +219,7 @@ const NavBar = ({ onSearch }) => {
           size="large"
           aria-label="mostrar carrito"
           color="inherit"
-          onClick={() => navigate('/cart')}
+          onClick={() => navigate("/cart")}
         >
           <Badge badgeContent={totalItemsInCart} color="error">
             <ShoppingCartIcon />
@@ -214,6 +239,18 @@ const NavBar = ({ onSearch }) => {
         </IconButton>
         <p>Perfil</p>
       </MenuItem>
+      {isAuthenticated && (
+        <MenuItem onClick={handleMyPosts}>
+          <IconButton
+            size="large"
+            aria-label="Mis Publicaciones"
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+          <p>Mis Publicaciones</p>
+        </MenuItem>
+      )}
       <MenuItem onClick={handleLogout}>
         <IconButton size="large" aria-label="Cerrar Sesión" color="inherit">
           <AccountCircle />
@@ -243,19 +280,19 @@ const NavBar = ({ onSearch }) => {
               variant="h6"
               noWrap
               component="div"
-              sx={{ display: { xs: 'none', sm: 'block' }, cursor: 'pointer' }}
-              onClick={() => navigate('/home')}
+              sx={{ display: { xs: "none", sm: "block" }, cursor: "pointer" }}
+              onClick={() => navigate("/home")}
             >
               PopMart
             </Typography>
-            
+
             <Search>
               <SearchIconWrapper>
                 <SearchIcon />
               </SearchIconWrapper>
               <StyledInputBase
                 placeholder="Buscar…"
-                inputProps={{ 'aria-label': 'search' }}
+                inputProps={{ "aria-label": "search" }}
                 value={searchText}
                 onChange={handleSearchChange}
                 onKeyDown={handleSearchSubmit}
@@ -263,17 +300,23 @@ const NavBar = ({ onSearch }) => {
             </Search>
 
             {isAuthenticated ? (
-              <Button color="inherit" onClick={() => navigate('/product/create')}>
+              <Button
+                color="inherit"
+                onClick={() => navigate("/product/create")}
+              >
                 Crear Producto
               </Button>
-            ):<Button disabled>
-                Crear Producto
-              </Button>
-            }
+            ) : (
+              <Button disabled>Crear Producto</Button>
+            )}
 
             <Box sx={{ flexGrow: 1 }} />
-            <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
-              <IconButton size="large" aria-label="mostrar mensajes" color="inherit">
+            <Box sx={{ display: { xs: "none", md: "flex" } }}>
+              <IconButton
+                size="large"
+                aria-label="mostrar mensajes"
+                color="inherit"
+              >
                 <Badge badgeContent={0} color="error">
                   <MailIcon />
                 </Badge>
@@ -287,12 +330,12 @@ const NavBar = ({ onSearch }) => {
                   <NotificationsIcon />
                 </Badge>
               </IconButton>
-              
+
               <IconButton
                 size="large"
                 aria-label="mostrar carrito"
                 color="inherit"
-                onClick={() => navigate('/cart')}
+                onClick={() => navigate("/cart")}
               >
                 <Badge badgeContent={totalItemsInCart} color="error">
                   <ShoppingCartIcon />
@@ -313,7 +356,7 @@ const NavBar = ({ onSearch }) => {
                 {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
               </IconButton>
             </Box>
-            <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+            <Box sx={{ display: { xs: "flex", md: "none" } }}>
               <IconButton
                 size="large"
                 aria-label="mostrar más"
